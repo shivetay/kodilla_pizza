b642721ee058457dff9602933a82b4a8670abd96
Łukasz Dawidowicz <lddev@outlook.com>
2019-07-31 09:50:50 +0200

    commit after 9.0 module

--- src/js/script.js
+++ src/js/script.js
@@ -5,16 +5,17 @@
   
     const select = {
       templateOf: {
-        menuProduct: '#template-menu-product'
+        menuProduct: '#template-menu-product',
+        cartProduct: '#template-cart-product', // CODE ADDED
       },
       containerOf: {
         menu: '#product-list',
-        cart: '#cart'
+        cart: '#cart',
       },
       all: {
         menuProducts: '#product-list > .product',
         menuProductsActive: '#product-list > .product.active',
-        formInputs: 'input, select'
+        formInputs: 'input, select',
       },
       menuProduct: {
         clickable: '.product__header',
@@ -22,36 +23,67 @@
         priceElem: '.product__total-price .price',
         imageWrapper: '.product__images',
         amountWidget: '.widget-amount',
-        cartButton: '[href="#add-to-cart"]'
+        cartButton: '[href="#add-to-cart"]',
       },
       widgets: {
         amount: {
-          input: 'input[name="amount"]',
+          input: 'input.amount', // CODE CHANGED
           linkDecrease: 'a[href="#less"]',
-          linkIncrease: 'a[href="#more"]'
-        }
-      }
+          linkIncrease: 'a[href="#more"]',
+        },
+      },
+      // CODE ADDED START
+      cart: {
+        productList: '.cart__order-summary',
+        toggleTrigger: '.cart__summary',
+        totalNumber: `.cart__total-number`,
+        totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+        subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+        deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+        form: '.cart__order',
+        formSubmit: '.cart__order [type="submit"]',
+        phone: '[name="phone"]',
+        address: '[name="address"]',
+      },
+      cartProduct: {
+        amountWidget: '.widget-amount',
+        price: '.cart__product-price',
+        edit: '[href="#edit"]',
+        remove: '[href="#remove"]',
+      },
+      // CODE ADDED END
     };
-  
+    
     const classNames = {
       menuProduct: {
         wrapperActive: 'active',
-        imageVisible: 'active'
-      }
+        imageVisible: 'active',
+      },
+      // CODE ADDED START
+      cart: {
+        wrapperActive: 'active',
+      },
+      // CODE ADDED END
     };
-  
+    
     const settings = {
       amountWidget: {
         defaultValue: 1,
         defaultMin: 1,
-        defaultMax: 9
-      }
+        defaultMax: 9,
+      }, // CODE CHANGED
+      // CODE ADDED START
+      cart: {
+        defaultDeliveryFee: 20,
+      },
+      // CODE ADDED END
     };
-  
+    
     const templates = {
-      menuProduct: Handlebars.compile(
-        document.querySelector(select.templateOf.menuProduct).innerHTML
-      )
+      menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+      // CODE ADDED START
+      cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+      // CODE ADDED END
     };
   
     /*Product object */
@@ -122,7 +154,7 @@
         thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
         thisProduct.amountWidgetElem.addEventListener('update', function(){
           thisProduct.processOrder();
-        })
+        });
     }
 
       /* init accordion */
@@ -206,13 +238,13 @@
           }
           /*TODO: FIXME: image if block */
           const imgClass = '.' + paramId + '-' + optionId;
+          console.log('**', imgClass);
           const img = thisProduct.imageWrapper.children;
           console.log('*****', img);
             if(img === optionSelected && img === imgClass){
               for(let imgs of img){
-                console.log('********imgs', imgs);
                 imgs.classList.add(classNames.menuProduct.imageVisible);
-                console.log('********imgs', imgs);
+                console.log('***imgs', imgs);
               }
             }
             else {
