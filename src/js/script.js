ad5e2d44f405c4ad0c28da1386e5c2bd32eee232
Łukasz Dawidowicz <lddev@outlook.com>
2019-07-31 17:26:31 +0200

    add event handler on cart button

--- src/js/script.js
+++ src/js/script.js
@@ -327,6 +327,35 @@
         thisWidget.element.dispatchEvent(event);
       }
     }
+
+    class Cart{
+      constructor(element){
+        const thisCart = this;
+
+        thisCart.products = [];
+
+        thisCart.getElements(element);
+        thisCart.initActions();
+
+        console.log('new Cart', thisCart);
+      }
+
+      getElements(element){
+        const thisCart = this;
+        thisCart.dom = {};
+
+        thisCart.dom.wrapper = element;
+        thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+      }
+      initActions(){
+      const  thisCart = this;
+
+        thisCart.dom.toggleTrigger.addEventListener('click', function(e){
+          e.preventDefault();
+          thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+        });
+      }
+    }
     
     const app = {
       productRef: [],
@@ -363,6 +392,13 @@
   
         thisApp.initData();
         thisApp.initMenu();
+        thisApp.initCart();
+      },
+      initCart: function() {
+        const thisApp = this;
+
+        const cartElem = document.querySelector(select.containerOf.cart);
+        thisApp.cart = new Cart(cartElem);
       }
     };
   
