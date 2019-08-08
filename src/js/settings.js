63827dbb81965af2464dc5957ce527e696570e96
Łukasz Dawidowicz <lddev@outlook.com>
2019-08-08 08:36:25 +0200

    add chnages from module 10.2

--- src/js/settings.js
+++ src/js/settings.js
@@ -5,10 +5,13 @@ export const select = {
     templateOf: {
       menuProduct: '#template-menu-product',
       cartProduct: '#template-cart-product', // CODE ADDED
+      bookingWidget: '#template-booking-widget',
     },
     containerOf: {
       menu: '#product-list',
       cart: '#cart',
+      pages: '#pages',
+      booking: '.booking-wrapper',
     },
     all: {
       menuProducts: '#product-list > .product',
@@ -28,6 +31,15 @@ export const select = {
         input: 'input.amount', // CODE CHANGED
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]',
+      },
+      datePicker: {
+        wrapper: '.date-picker',
+        input: `input[name="date"]`,
+      },
+      hourPicker: {
+        wrapper: '.hour-picker',
+        input: 'input[type="range"]',
+        output: '.output',
       },
     },
     // CODE ADDED START
@@ -50,6 +62,14 @@ export const select = {
       remove: '[href="#remove"]',
     },
     // CODE ADDED END
+    booking: {
+      peopleAmount: '.people-amount',
+      hoursAmount: '.hours-amount',
+      tables: '.floor-plan .table',
+    },
+    nav: {
+      links: '.main-nav a',
+    },
   };
   
 export  const classNames = {
@@ -62,24 +82,50 @@ export  const classNames = {
       wrapperActive: 'active',
     },
     // CODE ADDED END
+    booking: {
+      loading: 'loading',
+      tableBooked: 'booked',
+    },
+    nav: {
+      active: 'active',
+    },
+    pages: {
+      active: 'active',
+    },
   };
   
 export  const settings = {
+    hours: {
+      open: 12,
+      close: 24,
+    },
     amountWidget: {
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
     }, // CODE CHANGED
+    datePicker: {
+      maxDaysInFuture: 14,
+    },
     // CODE ADDED START
     cart: {
       defaultDeliveryFee: 20,
     },
     // CODE ADDED END
+    booking: {
+      tableIdAttribute: 'data-table',
+    },
     /* add local db */
     db: {
       url: '//localhost:3131',
       product: 'product',
       order: 'order',
+      booking: 'booking',
+      event: 'event',
+      dateStartParamKey: 'date_gte',
+      dateEndParamKey: 'date_lte',
+      notRepeatParam: 'repeat=false',
+      repeatParam: 'repeat_ne=false',
     },
   };
   
@@ -88,4 +134,5 @@ export  const templates = {
     // CODE ADDED START
     cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
     // CODE ADDED END
+    bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
   };
