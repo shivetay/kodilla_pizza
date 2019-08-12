156858e08fe21bb1142617259b936ef42bdb8bea
Łukasz Dawidowicz <lddev@outlook.com>
2019-08-12 10:27:55 +0200

    add minor changes, resolve eslint issue

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -12,12 +12,12 @@ class HourPicker extends BaseWidget {
 
         thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
         console.log('input', thisWidget.dom.input);
-        thisWidget.dom.outpot = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
+        thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
         console.log('output', thisWidget.dom.output);
 
         thisWidget.initPlugin();
-        thisWidget.dom.input = thisWidget.value;
-        console.log('input val', thisWidget.dom.input);
+        thisWidget.value = thisWidget.dom.input;
+        console.log('input val', thisWidget.value);
     }
     initPlugin(){
         const thisWidget = this;
@@ -25,26 +25,25 @@ class HourPicker extends BaseWidget {
         rangeSlider.create(thisWidget.dom.input);
         console.log('slider',rangeSlider);
         thisWidget.dom.input.addEventListener('input', function(){
-            rangeSlider = thisWidget.value;
-            console.log('slider value',rangeSlider);
-        });
+            thisWidget.value = thisWidget.dom.input;
+            console.log('widgte value', thisWidget.value);
+        })
+       
     }
     parseValue(value){
         utils.numberToHour(value);
-        console.log('par val', parVal);
-        console.log('val', value);
         return value;
     }
     isValid(value){
-        return value;
+        
       }
+
     renderValue(){
         const thisWidget = this;
         thisWidget.dom.output = thisWidget.value;
-        console.log('output render', thisWidget.dom.output);
+        console.log('outpu',thisWidget.dom.output);
     }
 
 }
-
 
 export default HourPicker;
